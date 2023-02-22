663eb3a7f2c89dca20ff7cf1a0f78e84dd1eff47
Alex <39212841+all4alex@users.noreply.github.com>
2023-02-23 03:14:10 +0800

    Updated login screen

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:fluttericon/zocial_icons.dart';
+import 'package:getwidget/getwidget.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -12,6 +15,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
         children: [
@@ -28,61 +32,171 @@ class _LoginScreenState extends State<LoginScreen> {
             child: Form(
               key: _formKey,
               child: Column(
+                mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  const Text(
-                    "Login",
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  Image.asset(
+                    'assets/icons/atly_splash_white_icon.png',
+                    width: 93.5,
+                    height: 93.4,
+                    fit: BoxFit.cover,
                   ),
-                  const SizedBox(height: 16.0),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Email",
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                        borderSide: BorderSide.none,
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Welcome to',
+                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                              fontFamily: 'Poppins',
+                              color: Colors.white,
+                            ),
                       ),
-                    ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Email can't be empty";
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Password",
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                        borderSide: BorderSide.none,
+                      Image.asset(
+                        'assets/icons/atly_splash_white_text_icon.png',
+                        width: 73.8,
+                        height: 50.2,
+                        fit: BoxFit.cover,
                       ),
+                    ],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          width: double.infinity,
+                          child: Text(
+                            'Log In',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .copyWith(
+                                  fontFamily: 'Poppins',
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                          ),
+                        ),
+                        const SizedBox(height: 16.0),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "Email",
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Email can't be empty";
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 16.0),
+                        TextFormField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            hintText: "Password",
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Password can't be empty";
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 10.0),
+                        Container(
+                          alignment: Alignment.centerRight,
+                          width: double.infinity,
+                          child: Text(
+                            'Forgot password?',
+                            style:
+                                Theme.of(context).textTheme.bodySmall!.copyWith(
+                                      fontFamily: 'Poppins',
+                                      color: Colors.white,
+                                      decoration: TextDecoration.underline,
+                                    ),
+                          ),
+                        ),
+                        const SizedBox(height: 16.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              // Log the user in
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              fixedSize: Size(screenSize.width, 50),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50))),
+                          child: Text('Log In',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(
+                                    fontFamily: 'Poppins',
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold,
+                                  )),
+                        ),
+                      ],
                     ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Password can't be empty";
-                      }
-                      return null;
-                    },
                   ),
-                  const SizedBox(height: 16.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        // Log the user in
-                      }
-                    },
-                    child: const Text("Login"),
+                  const SizedBox(height: 10.0),
+                  Container(
+                    alignment: Alignment.center,
+                    width: double.infinity,
+                    child: Text(
+                      'OR',
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            fontFamily: 'Poppins',
+                            color: Colors.white,
+                          ),
+                    ),
                   ),
+                  const SizedBox(height: 10.0),
+                  Column(
+                    children: [
+                      SignInButton(
+                        Buttons.Email,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                        onPressed: () {},
+                      ),
+                      SignInButton(
+                        Buttons.Google,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                        onPressed: () {},
+                      ),
+                      SignInButton(
+                        Buttons.Facebook,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                        onPressed: () {},
+                      ),
+                      SignInButton(
+                        Buttons.Apple,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                        onPressed: () {},
+                      ),
+                    ],
+                  )
                 ],
               ),
             ),
